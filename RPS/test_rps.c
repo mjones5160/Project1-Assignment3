5da5c228cca750d6ced63e4dd6065cc0aa558f91
Matthew Jones <Mjones5160@conestogac.on.ca>
2024-10-30 22:57:35 -0400

    Filled out test platform

--- RPS/test_rps.c
+++ RPS/test_rps.c
@@ -5,6 +5,10 @@
 const char* determine_winner(const char* player1, const char* player2);
 
 void test_determine_winner() {
-
-
+	assert(strcmp(determine_winner("Rock", "Scissors"), "Player1") == 0);
+	assert(strcmp(determine_winner("Scissors", "Paper"), "Player1") == 0);
+	assert(strcmp(determine_winner("Paper", "Rock"), "Player1") == 0);
+	assert(strcmp(determine_winner("Rock", "Rock"), "Draw") == 0);
+	assert(strcmp(determine_winner("Rock", "Paper"), "Player2") == 0);
+	assert(strcmp(determine_winner("Invalid", "Rock"), "Invalid") == 0);
 }
