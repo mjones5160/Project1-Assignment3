ed43e15725acd95866d36ae0157c6be5d9563ff7
Matthew Jones <Mjones5160@conestogac.on.ca>
2024-10-30 22:49:03 -0400

    Implemented determine winner function

--- RPS/rps.c
+++ RPS/rps.c
@@ -5,4 +5,14 @@ const char* determine_winner(const char* player1, const char* player2) {
         (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0)) {
         return "Invalid";
     }
+    if (strcmp(player1, player2) == 0) {
+        return "Draw";
+    }
+
+    if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
+        (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0) ||
+        (strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0)) {
+        return "Player1";
+    }
+    return "Player2";
 }
